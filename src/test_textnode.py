8a3c5d7ea6728a636df156e865320f24dcf70c19
Niviat <uniwodrafox@yahoo.ca>
2026-05-06 02:31:20 +0200

    Added unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,32 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq_simple(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("Anchor text", TextType.LINK, "https://www.google.com")
+        node2 = TextNode("Anchor text", TextType.LINK, "https://www.google.com")
+        self.assertEqual(node, node2)
+
+    def test_neq_simple(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_neq_url(self):
+        node = TextNode("Anchor text", TextType.LINK, "https://www.google.com")
+        node2 = TextNode("Anchor text", TextType.IMAGE, "https://www.google.com")
+        self.assertNotEqual(node, node2)
+
+    def test_neq_url_None(self):
+        node = TextNode("Anchor text", TextType.LINK)
+        node2 = TextNode("Anchor text", TextType.LINK, "")
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
